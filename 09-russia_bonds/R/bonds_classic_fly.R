5f0cc6a829161398ece0197dea3a9ca1e995602a
nick <nf_nazarov@mail.ru>
2016-08-31 00:30:36 +0300

    2 coures correct 4

--- 09-russia_bonds/R/bonds_classic_fly.R
+++ 09-russia_bonds/R/bonds_classic_fly.R
@@ -11,7 +11,7 @@ library(parallel)
 #    install.packages("XLConnect")
 #    library(XLConnect)
 #}
-source("09-bonds/R/reality_func2.R")
+source("~/workdir/09-russia_bonds/R/reality_func2.R")
 RESULT_PATH <- "~/workdir/"
 #rankingFactor <- 0
 
